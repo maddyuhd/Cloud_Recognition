9c32947a5768181de2db8ba24ee214cfee87c879
maddyuhd <krishnanmadhavan000@gmail.com>
2016-12-07 12:21:01 +0530

    minor changes

--- Online_test.py
+++ Online_test.py
@@ -2,7 +2,6 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 import time
-from keypoints import drawkeyPointsOnImage 
 import sys
 import os
 
@@ -28,29 +27,45 @@ def resize(h1,w1,res,scn_img):
 	scn_img = cv2.resize(scn_img, (newW, newH))
 	return scn_img
 
-def match(scn_img,imagePath1,filename,kp1):
+def maskdata(scn_img,h,w):
+
+	mask =  np.zeros(scn_img.shape[:2], dtype = "uint8")
+
+	h=h-10
+	w=w-15
+
+	mask = np.zeros(scn_img.shape,np.uint8)
+	mask[10:h,15:w] = scn_img[10:h,15:w]
+	masked_data = cv2.bitwise_and(scn_img, scn_img, mask=mask)
+	return masked_data
+
+def result(imagePath,loop,pos,neg,a):
+
+	print(str(imagePath) +" ====> "+"Compared with "+str(loop) +" images")
+	print("No. of positive matches = "+str(len(pos)))
+	print '\n'.join(str(p) for p in pos)
+	print("No. of negative matches = "+str(len(neg)))
+	print '\n'.join(str(p) for p in neg)
+	print "Total time (server)- ",str(sum(a))
+
+def match(scn_img,imagePath1,filename,des1):
 		src_img = cv2.imread(imagePath1,0)
 	#==============================================>Resize
 		h2,w2 = src_img.shape[:2]
 		src_img=resize(h2,w2,320,src_img)
 		#2448
-
 	#==============================================>KPT
 
 		# kp2 = fast.detect(src_img,None)
 		kp2, descs = detector.detectAndCompute(src_img, None)
 		# print("input:",len(kp2))
-
 	#==============================================>DRAW KPT
 
 		img5 = cv2.drawKeypoints(src_img, kp2,None, color=(0,255,255))
 		cv2.imwrite('k.jpg', img5)
 	#==============================================>FREAK_DES
 
-		freakExtractor = cv2.xfeatures2d.FREAK_create()
-
 		kp2,des2= freakExtractor.compute(src_img,kp2)
-	
 	#==============================================>FLANN MATCHING
 		start_time = time.time()
 
@@ -61,8 +76,7 @@ def match(scn_img,imagePath1,filename,kp1):
 		matcher = cv2.FlannBasedMatcher(flann_params, {})
 		if(len(kp2)>10):
 			matches=matcher.knnMatch(np.asarray(des1,np.float32),np.asarray(des2,np.float32), 2)
-
-		#==============================================>GOOD MATCHING
+	#==============================================>GOOD MATCHING
 
 			good = []
 			for m,n in matches:
@@ -116,51 +130,34 @@ def match(scn_img,imagePath1,filename,kp1):
 		else:
 			print "error - "+str(filename)
 
-def result(imagePath,loop,pos,neg,a):
 
-	print(str(imagePath) +" ====> "+"Compared with "+str(loop) +" images")
-	print("No. of positive matches = "+str(len(pos)))
-	print '\n'.join(str(p) for p in pos)
-	print("No. of negative matches = "+str(len(neg)))
-	print '\n'.join(str(p) for p in neg)
-	print "Total time (server)- ",str(sum(a))
-
+#==============================================>Input
 try:
-	#==============================================>Input
 	imagePath = sys.argv[1]
 	scn_img = cv2.imread(imagePath,0)
 
 except :
-	imagePath="pics/boat1.jpg"
+	imagePath="pics/box2.jpg"
 	# imagePath="/home/smacar/Desktop/dev/online/db/movingobjects/barrywhitemov/barrywhitemovVGA_00001.jpg"
 	scn_img= cv2.imread(imagePath,0)
 
 path = '/home/smacar/Desktop/dev/online/pics/src'
 # path="/home/smacar/Desktop/dev/online/db/database"
 
-#==============================================>Constants
-# fast = cv2.FastFeatureDetector_create(49)
-detector=cv2.xfeatures2d.SURF_create(400, 5, 5)
 
 h1,w1 = scn_img.shape[:2]
 scn_img=resize(h1,w1,1280,scn_img)
 
-#==============================================>MASK
+masked_data = maskdata(scn_img,h1,w1)
 
-mask =  np.zeros(scn_img.shape[:2], dtype = "uint8")
-
-h,w = scn_img.shape[:2]
-h=h-10
-w=w-15
-
-mask = np.zeros(scn_img.shape,np.uint8)
-mask[10:h,15:w] = scn_img[10:h,15:w]
-masked_data = cv2.bitwise_and(scn_img, scn_img, mask=mask)
-
-
+# fast = cv2.FastFeatureDetector_create(49)
+detector=cv2.xfeatures2d.SURF_create(400, 5, 5)
 
 kp1, desc = detector.detectAndCompute(scn_img, masked_data)
+
 # kp1 = fast.detect(scn_img,masked_data)
+freakExtractor = cv2.xfeatures2d.FREAK_create()
+kp1,des1= freakExtractor.compute(scn_img,kp1)
 
 img4 = cv2.drawKeypoints(scn_img, kp1,None, color=(0,255,255))
 cv2.imwrite('k_input.jpg', img4)
@@ -169,6 +166,6 @@ cv2.imwrite('k_input.jpg', img4)
 for filename in os.listdir(path):
 	loop+=1
 	imagePath1=path+"/"+filename
-	match(scn_img,imagePath1,filename,kp1)
+	match(scn_img,imagePath1,filename,des1)
 
 result(imagePath,loop,pos,neg,a)
